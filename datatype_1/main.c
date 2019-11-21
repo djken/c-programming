37a91cb64ce7fdc9228645a428548af2d4cc1ecc
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-11-20 20:18:52 -0500

    datatype

---
+++ datatype_1/main.c
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: kendy
+ *
+ * Created on November 20, 2019, 6:22 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(){
+    printf("int: %d \n", sizeof(int));
+    printf("float: %d \n", sizeof(float));
+    printf("double: %d \n", sizeof(double));
+    printf("char: %d \n", sizeof(char));
+    return 0;
+}
+
