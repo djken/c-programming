db05e2a8049c5f1ef6a73a05053bb4479ea7faa5
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-11-20 21:00:36 -0500

    constant

---
+++ 4_constant-1/main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: kendy
+ *
+ * Created on November 20, 2019, 8:53 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+
+int main(){
+    const double PI = 3.14;
+    printf("%f\n", PI);
+}
+
---
+++ 5_constant-2/main.c
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: kendy
+ *
+ * Created on November 20, 2019, 8:57 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+
+#define PI 3.14
+
+int main(){
+    printf("%f\n", PI);
+    
+    return 0;
+}
+
