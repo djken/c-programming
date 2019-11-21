bb6017537a376fc0bf10a1804cabbc0440583e14
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-11-20 20:52:08 -0500

    variable

---
+++ 3_variable/main.c
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: kendy
+ *
+ * Created on November 20, 2019, 8:45 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(){
+    int a, b;
+    float salary = 56.23;
+    char letter = 'Z';
+    
+    a = 8;
+    b = 34;
+    int c = a + b;
+    
+    printf("%d\n", c);
+    printf("%f\n", salary);
+    printf("%c\n", letter);
+    
+    return 0;
+}
